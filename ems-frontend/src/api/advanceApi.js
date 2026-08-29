d0369c63ac58a223562c09cfc2070d9aeff84681
Rama Mangtani <mangtanirama14@gmail.com>
2026-08-30 03:01:34 +0530

    Implement advance API functions

---
+++ ems-frontend/src/api/advanceApi.js
@@ -0,0 +1,8 @@
+import apiClient from './client';
+
+export const giveAdvance = (data) => apiClient.post('/advances', data);
+export const getOutstandingAdvances = () => apiClient.get('/advances');
+export const getAdvancesForEmployee = (employeeId) => apiClient.get(`/advances/employee/${employeeId}`);
+export const getOutstandingTotal = (employeeId) => apiClient.get(`/advances/employee/${employeeId}/total`);
+export const recordDeduction = (advanceId, amount) =>
+  apiClient.post(`/advances/${advanceId}/deduct`, null, { params: { amount } });
